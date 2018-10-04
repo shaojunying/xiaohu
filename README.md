08508c64c62f3410acf014ffd3ba59f24ac4a1fd
邵俊颖 <shaojunying@bupt.edu.cn>
2018-10-04 23:14:07 +0800

    implement user's register interface
    实现了用户注册接口

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # xiaohu
+
+### 一些笔记
+- php使用migrate创建数据库
+    - php artisan make:migration create_table_users 方法创建一个用户表
+    - php ../composer.phar dumpautoload 方法自动加载所有的类
+    - php artisan migrate 执行新创建的方法
+    - php artisan migrate:rollback 回滚到上一次执行
+- 创建用户表对应的类
+    - php artisan make:model User
--- xiaohu/app/User.php
+++ xiaohu/app/User.php
@@ -2,25 +2,40 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Request;
+use Hash;
 
-class User extends Authenticatable
+// dd(Request::get('age'));
+// dd(Request::has('username'));
+// dd(Request::all());
+class User extends Model
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    public function signup(){
+        $username = Request::get('username');
+        $password = Request::get('password');
+        // 检查用户名和密码是否为空
+        if (!($username && $password)) {
+            return ['status'=>0,'msg'=>'用户名和密码都不可为空'];
+        }
+        // 检查用户名是否存在
+        $user_exists = $this
+        ->where('username',$username)
+        ->exists();
+        if ($user_exists) {
+            return ['status'=>0,'msg'=>'用户名已存在'];
+        }
+        // 将密码加密
+        $hashed_password = Hash::make($password);
+        // 存入数据库
+        $user = $this;
+        $user->password = $hashed_password;
+        $user->username = $username;
+        if($user->save()){
+            return ['status'=>1,'id'=>$user->id];
+        }else {
+            return ['status'=>0,'msg'=>'db insert failed'];
+        }
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+    }
 }
