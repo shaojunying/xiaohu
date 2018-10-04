95fff5d14bfef3557a82b5f76fd480e01f976938
邵俊颖 <shaojunying@bupt.edu.cn>
2018-10-04 22:27:01 +0800

    set up user table
    set up user table

---
+++ xiaohu/database/migrations/2018_10_04_141256_create_table_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('username')->unique();
+            $table->string('email')->unique()->nullable();
+            $table->text('avatar_id_url')->nullable();
+            $table->string('phone')->unique()->nullable(); // +8612345678901
+            $table->string('password');
+            $table->text('intro')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('users');
+    }
+}
