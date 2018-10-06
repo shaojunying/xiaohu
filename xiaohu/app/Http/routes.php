5b707f936df3c7dea1aeda55d46426375034f2e2
邵俊颖 <shaojunying@bupt.edu.cn>
2018-10-06 10:12:41 +0800

    set up answers addition, edit, read, removal api
    Signed-off-by: 邵俊颖 <shaojunying@bupt.edu.cn>

--- xiaohu/app/Http/routes.php
+++ xiaohu/app/Http/routes.php
@@ -23,6 +23,11 @@ function user_ins(){
 function question_ins(){
     return new App\Question;
 }
+
+function answer_ins()
+{
+    return new App\Answer;
+}
 Route::get('/', function () {
     return view('welcome');
 });
@@ -49,6 +54,18 @@ Route::any('api/question/read',function(){
 });
 Route::any('api/question/remove',function(){
     return question_ins()->remove();
+});
+Route::any('api/answer/add', function () {
+    return answer_ins()->add();
+});
+Route::any('api/answer/change', function () {
+    return answer_ins()->change();
+});
+Route::any('api/answer/read', function () {
+    return answer_ins()->read();
+});
+Route::any('api/answer/remove', function () {
+    return answer_ins()->remove();
 });
 
 Route::any('test',function(){
